b4f87571b07cf4a2018fcca58f9060f01cfa5739
valex <valex@localhost>
2007-08-17 18:07:11 +0000

    batch compilation

---
+++ _compile.bat
@@ -0,0 +1,9 @@
+@echo off
+msdev ac3enc.dsp     /MAKE "ac3enc - Win32 Release"
+msdev bsconvert.dsp  /MAKE "bsconvert - Win32 Release"
+msdev mpeg_demux.dsp /MAKE "mpeg_demux - Win32 Release"
+msdev noise.dsp      /MAKE "noise - Win32 Release"
+msdev spdifer.dsp    /MAKE "spdifer - Win32 Release"
+msdev swab.dsp       /MAKE "swab - Win32 Release"
+msdev valdec.dsp     /MAKE "valdec - Win32 Release"
+call _clear.bat
