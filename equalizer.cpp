851a07d8203a3d537dcf8100608006b63dd75ce3
valex <valex@localhost>
2008-06-29 11:54:33 +0000

    equalizer: dither option

--- equalizer.cpp
+++ equalizer.cpp
@@ -1,7 +1,9 @@
 #include "source/wav_source.h"
 #include "sink/sink_wav.h"
 #include "filter_graph.h"
+#include "filters/agc.h"
 #include "filters/convert.h"
+#include "filters/dither.h"
 #include "filters/equalizer.h"
 #include "vtime.h"
 #include "vargs.h"
@@ -31,6 +33,7 @@ int main(int argc, char **argv)
 "  output.wav - file to write the result to\n"
 "  -fx - center frequency in Hz for band x (100 bands max)\n"
 "  -gx - gain in dB for band x (100 bands max)\n"
+"  -dither - dither the result\n"
 "\n"
 "If gain for a band specified with -fx parameter is not set, 0dB is assumed\n"
 "\n"
@@ -46,6 +49,7 @@ int main(int argc, char **argv)
   const char *output_filename = argv[2];
   int freq[max_bands];
   double gain[max_bands];
+  bool do_dither = false;
 
   for (i = 0; i < max_bands; i++) freq[i] = 0, gain[i] = 0;
 
@@ -54,32 +58,41 @@ int main(int argc, char **argv)
 
   for (int iarg = 3; iarg < argc; iarg++)
   {
-    // -fx -gx
-    for (i = 0; i < max_bands; i++)
+    // -dither
+    if (is_arg(argv[iarg], "dither", argt_bool))
     {
-      char buf[10];
-      sprintf(buf, "f%i", i);
-      if (is_arg(argv[iarg], buf, argt_num))
-      {
-        freq[i] = (int)arg_num(argv[iarg]);
-        break;
-      }
-
-      sprintf(buf, "g%i", i);
-      if (is_arg(argv[iarg], buf, argt_num))
-      {
-        if (freq[i] == 0)
-        {
-          printf("Unknown freqency for band %i (define the band's frequency before the gain)\n", argv[iarg]);
-          return -1;
-        }
-        gain[i] = arg_num(argv[iarg]);
-        break;
-      }
-    }
-
-    if (i < max_bands)
+      do_dither = arg_bool(argv[iarg]);
       continue;
+    }
+    else
+    {
+      // -fx -gx
+      for (i = 0; i < max_bands; i++)
+      {
+        char buf[10];
+        sprintf(buf, "f%i", i);
+        if (is_arg(argv[iarg], buf, argt_num))
+        {
+          freq[i] = (int)arg_num(argv[iarg]);
+          break;
+        }
+
+        sprintf(buf, "g%i", i);
+        if (is_arg(argv[iarg], buf, argt_num))
+        {
+          if (freq[i] == 0)
+          {
+            printf("Unknown freqency for band %i (define the band's frequency before the gain)\n", i);
+            return -1;
+          }
+          gain[i] = arg_num(argv[iarg]);
+          break;
+        }
+      }
+
+      if (i < max_bands)
+        continue;
+    }
 
     printf("Error: unknown option: %s\n", argv[iarg]);
     return -1;
@@ -96,6 +109,12 @@ int main(int argc, char **argv)
       gain[nbands] = gain[i];
       nbands++;
     }
+
+  if (nbands < max_bands)
+  {
+    freq[nbands] = 0;
+    gain[nbands] = 0;
+  }
 
   if (nbands == 0)
   {
@@ -130,23 +149,34 @@ int main(int argc, char **argv)
   Speakers spk = src.get_output();
 
   /////////////////////////////////////////////////////////////////////////////
-  // Do the job
+  // Build the chain
 
   Converter iconv(block_size);
   Converter oconv(block_size);
   iconv.set_format(FORMAT_LINEAR);
   oconv.set_format(src.get_output().format);
 
-  Equalizer filter;
-  if (!filter.set_bands(nbands, freq, gain))
+  Equalizer eq;
+  eq.set_enabled(true);
+  if (!eq.set_bands(nbands, freq, gain))
   {
     printf("Bad band parameters\n");
     return -1;
   }
 
+  AGC agc(1024);
+  Dither dither;
   FilterChain chain;
+
   chain.add_back(&iconv, "Input converter");
-  chain.add_back(&filter, "Equalizer");
+  chain.add_back(&eq, "Equalizer");
+  if (do_dither)
+  {
+    chain.add_back(&dither, "Dither");
+    if (spk.level > 128.0)
+      dither.level = 0.5 / spk.level;
+  }
+  chain.add_back(&agc, "AGC");
   chain.add_back(&oconv, "Output converter");
 
   if (!chain.set_input(spk))
@@ -154,6 +184,9 @@ int main(int argc, char **argv)
     printf("Error: cannot start processing\n");
     return -1;
   }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Do the job
 
   Chunk chunk;
   printf("0%%\r");
