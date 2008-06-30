bf3a4e657ac4375d311443847296b1b9ae2a67c7
valex <valex@localhost>
2008-06-30 14:27:35 +0000

    wavdiff: check for maximum difference allowed

--- wavdiff.cpp
+++ wavdiff.cpp
@@ -18,25 +18,30 @@ int main(int argc, char **argv)
 
 "WAVDiff\n"
 "=======\n"
-"Find the difference between 2 audio files. Calculates maximum amplitude\n"
-"difference and RMS difference. Can compare files of different types, like\n"
-"PCM16 and PCM Float (number of channels and sample rate must match).\n"
-"Can write the difference into the difference file."
+"Find the difference between 2 audio files. Calculates amplitude difference\n"
+"and RMS difference. Can compare files of different types, like PCM16 and\n"
+"PCM Float (number of channels and sample rate must match). Can write\n"
+"the difference into the difference file. May check maximum difference for\n"
+"automated testing\n"
 "\n"
 "This utility is a part of AC3Filter project (http://ac3filter.net)\n"
 "Copyright (c) 2008 by Alexander Vigovsky\n"
 "\n"
 "Usage:\n"
-"  > wavdiff a.wav b.wav [-diff c.wav]\n"
+"  > wavdiff a.wav b.wav [-diff c.wav] [-max_diff:n] [-max_rms:n] [-max_mean:n]\n"
 "\n"
 "Options:\n"
 "  a.wav - First file to compare\n"
 "  b.wav - Second file to compare\n"
 "  c.wav - Difference file\n"
+"  -max_diff - maximum difference between files allowed (dB)\n"
+"  -max_rms  - maximum RMS difference between files allowed (dB)\n"
+"  -max_mean - maximum mean difference between files allowed (dB)\n"
 "\n"
 "Example:\n"
-" > wavdiff a.wav b.wav -diff c.wav\n"
+" > wavdiff a.wav b.wav -diff c.wav -max_rms:-90\n"
 " Comapre a.wav and b.wav and write the difference into c.wav\n"
+" If the difference between files is more than -90dB exit code will be non-zero\n"
     );
     return 0;
   }
@@ -44,16 +49,39 @@ int main(int argc, char **argv)
   const char *filename1 = argv[1];
   const char *filename2 = argv[2];
   const char *diff_filename = 0;
-  double min_rms = 0;
-  double max_rms = 0;
-  double min_diff = 0;
-  double max_amp = 0;
+  bool check_diff = false;
+  bool check_rms  = false;
+  bool check_mean = false;
+  double max_diff = 0;
+  double max_rms  = 0;
+  double max_mean = 0;
 
   /////////////////////////////////////////////////////////////////////////////
   // Parse arguments
 
   for (int iarg = 3; iarg < argc; iarg++)
   {
+    if (is_arg(argv[iarg], "max_diff", argt_num))
+    {
+      max_diff = arg_num(argv[iarg]);
+      check_diff = true;
+      continue;
+    }
+
+    if (is_arg(argv[iarg], "max_mean", argt_num))
+    {
+      max_mean = arg_num(argv[iarg]);
+      check_mean = true;
+      continue;
+    }
+
+    if (is_arg(argv[iarg], "max_rms", argt_num))
+    {
+      max_rms = arg_num(argv[iarg]);
+      check_rms = true;
+      continue;
+    }
+
     if (is_arg(argv[iarg], "diff", argt_exist))
     {
       if (argc - iarg < 1)
@@ -69,6 +97,8 @@ int main(int argc, char **argv)
     printf("Error: unknown option: %s\n", argv[iarg]);
     return -1;
   }
+
+  printf("Comparing %s - %s...\n", filename1, filename2);
 
   /////////////////////////////////////////////////////////////////////////////
   // Open files
@@ -144,7 +174,7 @@ int main(int argc, char **argv)
 
   SourceFilter src1(&f1, &conv1);
   SourceFilter src2(&f2, &conv2);
-  SinkFilter diff(&f_diff, &conv_diff);
+  SinkFilter sink_diff(&f_diff, &conv_diff);
 
   /////////////////////////////////////////////////////////////////////////////
   // Do the job
@@ -154,7 +184,7 @@ int main(int argc, char **argv)
   Chunk chunk_diff;
   size_t n = 0;
 
-  double max_diff = 0.0;
+  double diff = 0.0;
   double mean = 0.0;
   double rms = 0.0;
 
@@ -197,11 +227,10 @@ int main(int argc, char **argv)
     for (ch = 0; ch < spk1.nch(); ch++)
       for (s = 0; s < len; s++)
       {
-        sample_t diff = chunk1.samples[ch][s] * norm1 - chunk2.samples[ch][s] * norm2;
-        if (fabs(diff) > max_diff)
-          max_diff = fabs(diff);
-        rms += diff * diff;
-        mean += diff;
+        sample_t d = chunk1.samples[ch][s] * norm1 - chunk2.samples[ch][s] * norm2;
+        if (fabs(d) > diff) diff = fabs(d);
+        mean += fabs(d);
+        rms += d * d;
       }
 
     ///////////////////////////////////////////////////////
@@ -219,7 +248,7 @@ int main(int argc, char **argv)
           chunk1.samples[ch][s] = chunk1.samples[ch][s] * norm1 - chunk2.samples[ch][s] * norm2;
 
       chunk_diff.set_linear(spk_diff_linear, chunk1.samples, len);
-      if (!diff.process(&chunk_diff))
+      if (!sink_diff.process(&chunk_diff))
       {
         printf("Error: cannot write to the difference file\n");
         return -1;
@@ -246,19 +275,37 @@ int main(int argc, char **argv)
   {
     chunk_diff.set_empty(spk_diff);
     chunk_diff.set_eos();
-    if (!diff.process(&chunk_diff))
+    if (!sink_diff.process(&chunk_diff))
     {
       printf("Error: cannot write to the difference file\n");
       return -1;
     }
   }
 
-  printf("100%%\n");
   rms = sqrt(rms/n);
   mean /= n;
 
-  printf("Max diff: %gdB\n", value2db(max_diff));
+  printf("    \r");
+  printf("Max diff: %gdB\n", value2db(diff));
   printf("RMS diff: %gdB\n", value2db(rms));
+  printf("Mean diff: %gdB\n", value2db(mean));
 
-  return 0;
+  int result = 0;
+  if (check_diff && diff > db2value(max_diff))
+  {
+    printf("Error: maximum difference is too large");
+    result = -1;
+  }
+  if (check_rms && rms > db2value(max_rms))
+  {
+    printf("Error: RMS difference is too large");
+    result = -1;
+  }
+  if (check_mean && mean > db2value(max_mean))
+  {
+    printf("Error: mean difference is too large");
+    result = -1;
+  }
+
+  return result;
 }
