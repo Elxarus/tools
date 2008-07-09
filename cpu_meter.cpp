f9825f49bcad69eb91141c758d8a933b839b4d19
valex <valex@localhost>
2008-07-09 07:54:47 +0000

    cpu_meter: new method

--- cpu_meter.cpp
+++ cpu_meter.cpp
@@ -1,6 +1,30 @@
+#include <windows.h>
 #include <stdio.h>
-#include <process.h>
-#include "win32/cpu.h"
+
+__int64 get_process_time(HANDLE process)
+{
+  __int64 creation_time;
+  __int64 exit_time;
+  __int64 kernel_time;
+  __int64 user_time;
+
+  if (GetProcessTimes(process, 
+         (FILETIME*)&creation_time, 
+         (FILETIME*)&exit_time, 
+         (FILETIME*)&kernel_time, 
+         (FILETIME*)&user_time))
+    return kernel_time + user_time;
+  return -1;
+}
+
+__int64 get_system_time()
+{
+  __int64 time;
+  SYSTEMTIME systime;
+  GetSystemTime(&systime);
+  SystemTimeToFileTime(&systime, (FILETIME*)&time);
+  return time;
+}
 
 int main(int argc, char *argv[])
 {
@@ -9,10 +33,10 @@ int main(int argc, char *argv[])
     printf(
 "cpu_meter\n"
 "=========\n"
-"Measure the CPU time consumed by a program run. Useful for automated testing.\n"
+"Measure the CPU time consumed by a program run.\n"
 "\n"
 "This utility is a part of AC3Filter project (http://ac3filter.net)\n"
-"Copyright (c) 2007 by Alexander Vigovsky\n"
+"Copyright (c) 2008 by Alexander Vigovsky\n"
 "\n"
 "Usage:\n"
 "  > cpu_meter program [arg1 [arg2 [...]]\n"
@@ -20,12 +44,33 @@ int main(int argc, char *argv[])
     return 0;
   }
 
-  CPUMeter cpu;
-  cpu.start();
-  _spawnv(_P_WAIT, argv[1], argv+1);
-  cpu.stop();
-  printf("\n");
-  printf("Program time: %ims\n", int(cpu.get_thread_time() * 1000));
-  printf("System time: %ims\n", int(cpu.get_system_time() * 1000));
-  return int(cpu.get_thread_time() * 1000);
+  LPTSTR command_line = GetCommandLine();
+  while (command_line[0] && command_line[0] != ' ' && command_line[0] != '\t') command_line++;
+  while (command_line[0] == ' ' || command_line[0] == '\t') command_line++;
+
+  STARTUPINFO si;
+  PROCESS_INFORMATION pi;
+  memset(&si, 0, sizeof(si));
+  si.cb = sizeof(si);
+
+  if (CreateProcess(0, command_line, 0, 0, TRUE, CREATE_SUSPENDED, 0, 0, &si, &pi) == 0)
+  {
+    printf("Cannot start the program\n");
+    return -1;
+  }
+  WaitForInputIdle(pi.hProcess, INFINITE);
+
+  __int64 process_time = get_process_time(pi.hProcess);
+  __int64 system_time = get_system_time();
+
+  ResumeThread(pi.hThread);
+  WaitForSingleObject(pi.hProcess, INFINITE);
+
+  process_time = get_process_time(pi.hProcess) - process_time;
+  system_time = get_system_time() - system_time;
+
+  printf("--------------------\n");
+  printf("Process time: %ims\n", int(process_time / 10000));
+  printf("System time: %ims\n", int(system_time / 10000));
+  return int(process_time / 10000);
 }
